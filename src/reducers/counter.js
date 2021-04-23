34dddbfc223e619b51eaf8c539ca8f317665df01
alstn6987 <alstn6987@gmail.com>
2021-04-23 11:26:56 +0900

    주석 삭제

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -16,14 +16,6 @@ const counterReducer = (state = {}, action) => {
     default:
       return state;
   }
-  // 함수조작이 길어질때
-  // if(action.type === 'INCREMENT'){
-  //     return state + 1;
-  // }else if(action.type === 'DECREMENT'){
-  //     return state -1;
-  // }else{asdasdsdasdasdasdasdasdd
-  //     return state;
-  // }
 };
 
 export default counterReducer;
